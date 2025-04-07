fb8dfda45311d0b9a1b3d2af3367dc793e8f97e0
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-04-07 15:20:25 +0500

    Updated

--- king/Sarkar/index.js
+++
@@ -1,5 +0,0 @@
-import Handler from './handler.js'
-import Callupdate from './call-handler.js'
-import GroupUpdate from './group-handler.js'
-import chatbotCommand from './chatbot.js'
-export { Handler, Callupdate, chatbotCommand, GroupUpdate };
